442116de3ed91d71dcf31ab6da8ec6e66b4caf0d
Johanna <85117688+JohannaWS@users.noreply.github.com>
2025-12-04 05:53:38 +0100

    move pipeline execution to run_pipeline

--- pipeline/setup.R
+++ pipeline/setup.R
@@ -1,7 +1,3 @@
-args <- commandArgs(trailingOnly = TRUE)
-if (length(args) == 0) stop("Please provide a year, e.g. Rscript pipeline/setup.R 2024")
-YEAR <- args[1]
-
 DIR_META <- file.path("data", "meta", YEAR)
 DIR_RAW <- file.path("data", "raw", YEAR)
 DIR_CLEANED <- file.path("data", "cleaned", YEAR)
@@ -13,14 +9,3 @@ for (d in dirs) {
     if (!dir.exists(d)) dir.create(d, recursive = TRUE)
 }
 
-source("pipeline/00-get-metadata.R")
-source("pipeline/01-get-og-data.R")
-source("pipeline/02-get-tn-data.R")
-
-if (file.exists(sprintf("pipeline/year_specific/process_og_%s.R", YEAR))) {
-  source(sprintf("pipeline/year_specific/process_og_%s.R", YEAR))
-} 
-if (file.exists(sprintf("pipeline/year_specific/process_tn_%s.R", YEAR))) {
-  source(sprintf("pipeline/year_specific/process_tn_%s.R", YEAR))
-}
-source("pipeline/03-delete-duplicates.R")
